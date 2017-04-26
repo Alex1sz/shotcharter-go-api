833f01bbe95d64c582491c15d721193cbb78f054
Alex Szabo <87szabo@gmail.com>
2017-04-26 13:52:07 -0700

    Makes Game.StartAt field sql.NullString needed for sqlx Scan() to work

--- models/game.go
+++ models/game.go
@@ -1,20 +1,21 @@
 package models
 
 import (
+	// database/sql import needed to use sql.NullString for sqlx Scan() functionality
+	"database/sql"
 	"github.com/alex1sz/shotcharter-go/db"
 	"log"
-	"time"
 )
 
 type Game struct {
-	ID        string    `db:"id" json:"id"`
-	StartAt   time.Time `db:"start_at" json:"start_at,omitempty"`
-	HomeTeam  *Team     `json:"home_team,omitempty"`
-	AwayTeam  *Team     `json:"away_team, omitempty"`
-	HomeScore uint8     `db:"home_score" json:"home_score"`
-	AwayScore uint8     `db:"away_score" json:"away_score"`
-	CreatedAt string    `db:"created_at" json:"created_at"`
-	UpdatedAt string    `db:"updated_at" json:"updated_at"`
+	ID        string         `db:"id" json:"id"`
+	StartAt   sql.NullString `db:"start_at" json:"start_at,omitempty"`
+	HomeScore uint8          `db:"home_score" json:"home_score"`
+	AwayScore uint8          `db:"away_score" json:"away_score"`
+	CreatedAt string         `db:"created_at" json:"created_at"`
+	UpdatedAt string         `db:"updated_at" json:"updated_at"`
+	HomeTeam  *Team          `db:"home_team_id" json:"home_team,omitempty"`
+	AwayTeam  *Team          `db:"away_team_id" json:"away_team,omitempty"`
 }
 
 func (game *Game) Create() (err error) {
@@ -23,18 +24,11 @@ func (game *Game) Create() (err error) {
 }
 
 func FindGameByID(id string) (game Game, err error) {
-	game = Game{}
-
-	// err = db.Db.QueryRowx("select (*) from games where id = $1", id).Scan(&game.ID, &game.StartAt, &game.HomeTeam, &game.AwayTeam, &game.HomeScore, &game.AwayScore, &game.CreatedAt, &game.UpdatedAt)
-	err = db.Db.Get(&game, "select (*) from games where id = $1", id)
+	err = db.Db.Get(&game, "select * from games where id = $1", id)
 
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
-	log.Print(game)
-	log.Println(game)
-
 	return game, err
 }
