cebd4808309323e5e5f66e04a78f143e0b31e72b
Alex Szabo <87szabo@gmail.com>
2017-04-25 18:15:23 -0700

    Adds InitRoutes & ListenAndServe calls to main

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"github.com/alex1sz/shotcharter-go/db"
+	// /"github.com/alex1sz/shotcharter-go/db"
+	"github.com/alex1sz/shotcharter-go/routers"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-	db.Db.Ping()
+	router := routers.InitRoutes()
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:8080",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Println("Now listening on port: 127.0.0.1:8080")
+	log.Fatal(server.ListenAndServe())
 }
