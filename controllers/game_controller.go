5b76abab10292af97ae9c3ea3da825c3353c224f
Alex Szabo <87szabo@gmail.com>
2017-05-12 11:47:23 -0700

    Add CreateGame to Game controller

--- controllers/game_controller.go
+++ controllers/game_controller.go
@@ -28,8 +28,33 @@ func GetGameByID(w http.ResponseWriter, req *http.Request) {
 		utils.RespondWithAppError(w, err, "An unexpected error has occurred", 500)
 		return
 	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResp)
+}
+
+// POST /games
+func CreateGame(w http.ResponseWriter, req *http.Request) {
+	var game models.Game
+	err := json.NewDecoder(req.Body).Decode(&game)
+
+	if err != nil {
+		utils.RespondWithAppError(w, err, "Invalid team data", 500)
+		return
+	}
+	game, err = game.Create()
+
+	if err != nil {
+		utils.RespondWithAppError(w, err, "An unexpected error has occurred", 500)
+		return
+	}
+	jsonResp, err := json.Marshal(game)
+
+	if err != nil {
+		utils.RespondWithAppError(w, err, "An unexpected error has occurred", 500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
 	w.Write(jsonResp)
 }
