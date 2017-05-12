61271fada1999b4b4ffe6b70a701e0b64dfb7576
Alex Szabo <87szabo@gmail.com>
2017-05-11 17:40:43 -0700

    Add shot router & controller

---
+++ controllers/shot_controller.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"encoding/json"
+	"github.com/alex1sz/shotcharter-go/utilities"
+	// gorilla/mux used for req params
+	// "github.com/gorilla/mux"
+	// "log"
+	"net/http"
+	// neccessary to catch sql.ErrNoRows
+	// "database/sql"
+
+	"github.com/alex1sz/shotcharter-go/models"
+)
+
+// POST /shots
+func CreateShot(w http.ResponseWriter, req *http.Request) {
+	// params := mux.Vars(req)
+	var shot models.Shot
+	err := json.NewDecoder(req.Body).Decode(&shot)
+
+	if err != nil {
+		utils.RespondWithAppError(w, err, "Invalid shot data", 500)
+		return
+	}
+	shotIsValid, err := shot.IsValid()
+
+	if !shotIsValid {
+		utils.RespondWithAppError(w, err, "Shot associations are not valid", 500)
+	}
+	shot.Create()
+	jsonResp, err := json.Marshal(shot)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(jsonResp)
+}
